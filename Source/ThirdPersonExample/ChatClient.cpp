423b183a91f6ef5c77e38855d648a481e78028f3
dexterlb <dexterlb@qtrp.org>
2017-06-26 16:01:55 +0300

    add AChatClient

---
+++ Source/ThirdPersonExample/ChatClient.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChatClient.h"
+
+
+// Sets default values
+AChatClient::AChatClient()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AChatClient::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AChatClient::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AChatClient::Connect()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Some debug message!"));
+}
+
---
+++ Source/ThirdPersonExample/ThirdPersonExample.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ThirdPersonExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThirdPersonExample, "ThirdPersonExample" );
